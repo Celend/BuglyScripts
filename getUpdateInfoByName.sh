7bbcda517063dde65158e1d26ee9c7220260c15e
TJT <tanjin3344@qq.com>
2016-11-04 21:00:03 +0800

    Simplify code

--- getUpdateInfoByName.sh
+++ getUpdateInfoByName.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+# Edit the jq_cmd to show more fields
 
 if [ "$#" = 0 ]; then
 	echo -e "Usage: ./[script] key\n"
@@ -7,17 +8,12 @@ if [ "$#" = 0 ]; then
 	exit
 fi
 
-key=$2
-
-if [ "$#" = 1 ]; then
-	appid=`tail -n 1 cookie`
-	key=$1
-fi
-
-cookie=`head -n 1 cookie`
+key=$1
+cookie=`sed '1!d' cookie`
+appid=`sed '2!d' cookie`
 
 jq_cmd="jq -C --indent 2 '.upgrade.allUpdate.items[][] | select(.title | test(\"${key}\")) | {title, issuedCount: .dataPo.issuedCount, activeCount: .dataPo.activeCount}'"
 
-curl_cmd="curl 'https://beta.bugly.qq.com/apps/${appid}/allupdate?pid=1' -s -H 'If-None-Match: W/\"12237-iKW9dNivmtEaZiYHh1fjsQ\"' -H 'Accept-Encoding: gzip, deflate, sdch, br' -H 'Accept-Language: en-GB,en;q=0.8,zh-CN;q=0.6,zh;q=0.4' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Cookie: ${cookie}' -H 'Connection: keep-alive' -H 'Cache-Control: max-age=0' --compressed"
+curl_cmd="curl 'https://beta.bugly.qq.com/apps/${appid}/allupdate?pid=1' -s -H 'Accept-Encoding: gzip, deflate, sdch, br' -H 'Accept-Language: en-GB,en;q=0.8,zh-CN;q=0.6,zh;q=0.4' -H 'Upgrade-Insecure-Requests: 1' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Cookie: ${cookie}' -H 'Connection: keep-alive' -H 'Cache-Control: max-age=0' --compressed"
 
 eval "$curl_cmd | perl -ne '/window\.__data=(.*?)<\/script>/ and print \$1, \"\n\"' | $jq_cmd" 2>/dev/null
